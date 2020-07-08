f3bd79cb81c33c76d45da5a3fef7b3819e645ae7
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-08 14:06:26 +0200

    Add BCryptHashService

---
+++ src/main/java/com/grades/tracker/api/services/BCryptHashService.java
@@ -0,0 +1,30 @@
+package com.grades.tracker.api.services;
+
+import com.grades.tracker.api.services.interfaces.HashService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BCryptHashService implements HashService {
+
+
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void setPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public String encode(String plain) {
+        return passwordEncoder.encode(plain);
+    }
+
+    @Override
+    public boolean valid(String raw, String encoded) {
+        return passwordEncoder.matches(raw, encoded);
+    }
+
+
+}
