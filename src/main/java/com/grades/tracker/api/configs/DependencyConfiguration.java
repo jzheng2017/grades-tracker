1ac1ade1e8b471ed1e87a551a7c5471d929cafa8
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-08 16:24:44 +0200

    Add ModelMapper Bean

--- src/main/java/com/grades/tracker/api/configs/DependencyConfiguration.java
+++ src/main/java/com/grades/tracker/api/configs/DependencyConfiguration.java
@@ -1,5 +1,6 @@
 package com.grades.tracker.api.configs;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,12 +8,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class DependencyConfiguration {
 
-//    @Bean
-//    public ModelMapper modelMapper() {
-//        ModelMapper modelMapper = new ModelMapper();
-//        modelMapper.getConfiguration().setAmbiguityIgnored(true);
-//        return modelMapper;
-//    }
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        return modelMapper;
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
