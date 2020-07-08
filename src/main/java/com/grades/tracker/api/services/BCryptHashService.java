90e4b8ffca7afb2d20acb30afca68b436b6be309
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-08 16:29:36 +0200

    Refactor spaces

--- src/main/java/com/grades/tracker/api/services/BCryptHashService.java
+++ src/main/java/com/grades/tracker/api/services/BCryptHashService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BCryptHashService implements HashService {
-
-
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
--- src/main/java/com/grades/tracker/api/services/KeyService.java
+++ src/main/java/com/grades/tracker/api/services/KeyService.java
@@ -11,7 +11,6 @@ import java.util.Base64;
 
 @Service
 public class KeyService {
-
     private AppConfig keyConfig;
 
     @Autowired
