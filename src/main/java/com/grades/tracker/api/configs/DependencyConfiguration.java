7723b8d5e2f09a194d11f5fb8cedb297ee25f9e1
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-08 14:07:21 +0200

    Add DependencyConfiguration

---
+++ src/main/java/com/grades/tracker/api/configs/DependencyConfiguration.java
@@ -0,0 +1,21 @@
+package com.grades.tracker.api.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class DependencyConfiguration {
+
+//    @Bean
+//    public ModelMapper modelMapper() {
+//        ModelMapper modelMapper = new ModelMapper();
+//        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+//        return modelMapper;
+//    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
