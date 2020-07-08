d334e9d975c0d1b58db251c68d62f2c3016460a5
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-08 14:08:11 +0200

    Add custom UserDetailsServiceImpl

---
+++ src/main/java/com/grades/tracker/api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.grades.tracker.api.security;
+
+import com.grades.tracker.api.entities.User;
+import com.grades.tracker.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            return new org.springframework.security.core.userdetails.User(user.get().getUsername(), user.get().getPassword(), Collections.emptyList());
+        }
+        throw new UsernameNotFoundException(username + " not found");
+    }
+
+}
