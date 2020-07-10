33563369ccdcf80f5b6595337abce61f43702787
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-10 16:59:08 +0200

    Add CRUD endpoints

--- src/main/java/com/grades/tracker/api/controllers/UserSchoolController.java
+++ src/main/java/com/grades/tracker/api/controllers/UserSchoolController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,9 +22,24 @@ public class UserSchoolController {
         this.userSchoolService = userSchoolService;
     }
 
-    @GetMapping("{userId}")
-    public ResponseEntity<List<UserSchoolDTO>> getAllSchoolsOfUser(@QuerydslPredicate(root = UserSchool.class) Predicate predicate, @PathVariable(name = "userId") int userId, Pageable pageable) {
-        return ResponseEntity.ok(userSchoolService.getAllSchoolsOfUser(predicate, pageable, userId));
+    @GetMapping
+    public ResponseEntity<List<UserSchoolDTO>> getAllUserSchools(@QuerydslPredicate(root = UserSchool.class) Predicate predicate, Pageable pageable) {
+        return ResponseEntity.ok(userSchoolService.getAllUserSchools(predicate, pageable));
     }
 
+    @PostMapping
+    public ResponseEntity<UserSchoolDTO> addUpdateUserSchool(@RequestBody UserSchoolDTO userSchoolDTO) {
+        return ResponseEntity.ok(userSchoolService.addUserSchool(userSchoolDTO));
+    }
+
+
+    @PutMapping
+    public ResponseEntity<UserSchoolDTO> updateUserSchool(@RequestBody UserSchoolDTO userSchoolDTO) {
+        return ResponseEntity.ok(userSchoolService.updateUserSchool(userSchoolDTO));
+    }
+
+    @DeleteMapping("{userSchoolId}")
+    public ResponseEntity<Boolean> deleteUserSchool(@PathVariable("userSchoolId") Integer userSchoolId) {
+        return ResponseEntity.ok(userSchoolService.deleteUserSchoolById(userSchoolId));
+    }
 }
