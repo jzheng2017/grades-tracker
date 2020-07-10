2712d792a44602ffe443f1e9712c910ac0aafb1e
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-10 15:40:57 +0200

    Add UserSchoolRepository

---
+++ src/main/java/com/grades/tracker/api/repositories/UserSchoolRepository.java
@@ -0,0 +1,6 @@
+package com.grades.tracker.api.repositories;
+
+import com.grades.tracker.api.entities.UserSchool;
+
+public interface UserSchoolRepository extends BaseRepository<UserSchool, Integer> {
+}
