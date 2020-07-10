a3d2bc3e37e1c3a7b208a1dbf35f7845ac92b617
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-10 15:41:14 +0200

    Add UserSchoolBranchRepository

---
+++ src/main/java/com/grades/tracker/api/repositories/UserSchoolBranchRepository.java
@@ -0,0 +1,6 @@
+package com.grades.tracker.api.repositories;
+
+import com.grades.tracker.api.entities.UserSchoolBranch;
+
+public interface UserSchoolBranchRepository extends BaseRepository<UserSchoolBranch, Integer> {
+}
