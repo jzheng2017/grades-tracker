0da5fb4d70af0bfc2f547ff41dcf871e4fee3f26
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-07-10 15:44:28 +0200

    Add UserSchoolDTO

---
+++ src/main/java/com/grades/tracker/api/dto/UserSchoolDTO.java
@@ -0,0 +1,51 @@
+package com.grades.tracker.api.dto;
+
+import java.time.LocalDateTime;
+
+public class UserSchoolDTO {
+    private Integer id;
+    private String schoolName;
+    private Integer userId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
